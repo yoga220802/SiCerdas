4fb610b6f4e3eed26eb6fd8a7ce0f422fbea5cde
yoga220802 <yogaagustiansyah01@gmail.com>
2025-05-29 23:35:39 +0700

    feat(onboarding): implement SplashScreen and FloatingCircle widget; remove placeholder test file

---
+++ lib/features/onboarding/view/get_started_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetStartedScreen extends StatefulWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  State<GetStartedScreen> createState() => _GetStartedScreenState();
+}
+
+class _GetStartedScreenState extends State<GetStartedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
