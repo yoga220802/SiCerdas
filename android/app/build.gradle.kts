5ccfe8f4eb050f1f2fe5b8ac9430937375394903
yoga220802 <yogaagustiansyah01@gmail.com>
2025-05-31 19:39:00 +0700

    feat(firebase): integrate Firebase services and configuration for analytics and database

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 android {
